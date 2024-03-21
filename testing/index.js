1cbbe370f1e5b7eeb0eee1e404d0036befa5a750
Rohit Karthik <rohitismaster@gmail.com>
2024-03-21 09:25:16 -0400

    updated tests

--- testing/index.js
+++ testing/index.js
@@ -1,17 +1,21 @@
 const { io } = require("socket.io-client");
 
-const socket = io("https://cs-final-project-server-ninjagor.koyeb.app/");
+// const socket = io("https://cs-final-project-server-dev-dqpk.1.us-1.fl0.io", { transports: ['websocket'] });
+const socket = io("http://localhost:3000", { transports: ['websocket'] });
 
-console.log(socket)
 
 let userId;
 
-socket.on('connection_details', (details) => {
-    console.log(details)
-    userId = details.userid;
-    changeUsername();
-    // socket.emit('create_room', "cool_room");
-    socket.emit('join_room', "cool_room")
+// socket.on('connection_details', (details) => {
+//     console.log(details)
+//     userId = details.userid;
+//     // changeUsername();
+//     // socket.emit('create_room', "cool_room");
+//     // socket.emit('join_room', "cool_room")
+// })
+
+socket.on('connection_details', (data) => {
+		console.log(data)
 })
 
 socket.on('error', (message) => {
@@ -46,13 +50,13 @@ function changeUsername() {
     // socket.emit('change_username', userId, 'ooga booga man');
 }
 
-const readline = require('node:readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-readline.question(`Enter chat message: `, message => {
-    console.log("CHAT SENT")
-    socket.emit('room_chat', message, "global")
-});
-
+// const readline = require('node:readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+//
+// readline.question(`Enter chat message: `, message => {
+//     console.log("CHAT SENT")
+//     socket.emit('room_chat', message, "global")
+// });
+//
