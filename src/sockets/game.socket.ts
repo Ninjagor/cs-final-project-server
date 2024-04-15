129e3643749a1b8907305e1c962fc3c9c1be8467
Rohit Karthik <rohitismaster@gmail.com>
2024-04-14 20:02:54 -0400

    added player info returning

--- src/sockets/game.socket.ts
+++ src/sockets/game.socket.ts
@@ -14,6 +14,7 @@ export default function gameSocket(io: Server) {
             gameDetails.push(new_player);
             socket.playerid = new_player.id;
             storage.set('SS', 'game_details', gameDetails);
+            socket.emit('return_player_info', new_player.id);
         })
 
         socket.on('get_game_details', () => {
