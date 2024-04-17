a5a7faf7917e0520088ce9a2045c904b9cd8782d
Rohit Karthik <rohitismaster@gmail.com>
2024-04-17 09:39:29 -0400

    reimplemented graceful disconnect

--- src/sockets/connection.socket.ts
+++ src/sockets/connection.socket.ts
@@ -86,9 +86,9 @@ export default function configureSocket(io: Server) {
             })
             storage.set('SS', 'users', filtered_users);
             if (socket.playerid) {
-                // let player_buffer = storage.get('SS', 'player_buffer');
-                // player_buffer.removePlayer(socket.playerid);
-                // storage.set('SS', 'player_buffer', player_buffer);
+                let player_buffer = storage.get('SS', 'player_buffer');
+                player_buffer.removePlayer(socket.playerid);
+                storage.set('SS', 'player_buffer', player_buffer);
             }
             if (socket.currentroom.roomname) {
                 let roomname = socket.currentroom.roomname;
